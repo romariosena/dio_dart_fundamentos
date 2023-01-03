0d6adb460e4d141eeca776bdae7b02deaaf3da89
Romário Sena <romariosena@outlook.com>
2023-01-03 18:43:33 -0300

    Primeiro Exerício usando funções isEven e isOdd

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,5 +1,16 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+  //print('Hello world: ${meu_app.calculate()}!');
+
+  int numero1 = 10;
+  var numero2 = 11;
+
+  print("Retorna verdadeiro se e somente se esse inteiro for par:");
+  print(numero1.isEven);
+  print(numero2.isEven);
+
+  print("Retorna verdadeiro se e somente se este inteiro for impar");
+  print(numero1.isOdd);
+  print(numero2.isOdd);
 }
